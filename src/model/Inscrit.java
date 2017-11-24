628b4ec80a2f5c4eebc7fc3b71212518b263e61d
kIRETA <robin.sgeorges@gmail.com>
2017-11-24 17:39:43 +0100

    Remove features "rechercher...()" for DAVID

--- src/model/Inscrit.java
+++ src/model/Inscrit.java
@@ -7,209 +7,212 @@ import java.util.List;
 import system.Gestion;
 
 public class Inscrit {
-    private String email;
+	private String email;
 
-    private long numero;
+	private long numero;
 
-    private String login;
+	private String login;
 
-    private String username;
+	private String username;
 
-    private String password;
+	private String password;
 
-    private String description;
+	private String description;
 
-    private List<Evenement> evenementsOrganises = new ArrayList<Evenement> ();
+	private List<Evenement> evenementsOrganises = new ArrayList<Evenement> ();
 
-    private List<Evenement> evenementsProposes = new ArrayList<Evenement> ();
+	private List<Evenement> evenementsProposes = new ArrayList<Evenement> ();
 
-    private List<Evenement> evenementsParticipes = new ArrayList<Evenement> ();
+	private List<Evenement> evenementsParticipes = new ArrayList<Evenement> ();
 
-    private List<Theme> themesPreferes = new ArrayList<Theme> ();
+	private List<Theme> themesPreferes = new ArrayList<Theme> ();
 
-    private Photo photo;
+	private Photo photo;
 
-    private List<Inscrit> followings = new ArrayList<Inscrit> ();
+	private List<Inscrit> followings = new ArrayList<Inscrit> ();
 
-    private static List<Inscrit> followers = new ArrayList<Inscrit> ();
+	private static List<Inscrit> followers = new ArrayList<Inscrit> ();
 
-    public Inscrit(final String email, final String login, final String username, final String password) {
-    }
+	public Inscrit(final String email, final String login, final String username, final String password) {
+	}
 
-    public Inscrit(final long numero, final String login, final String nom, final String password) {
-    }
+	public Inscrit(final long numero, final String login, final String nom, final String password) {
+	}
 
-    public void follow(final Inscrit following) {
-              this.followings.add(following);
-        
-              following.followers.add(this);
-    }
+	public void follow(final Inscrit following) {
+		this.followings.add(following);
 
-    public void unfollow(final Inscrit following) {
-    }
+		following.followers.add(this);
+	}
 
-    public boolean creerUnEvenement(final String nom, final Date dateDebut, final Date dateFin, final Lieu localisation, final Theme theme, final List<Inscrit> invites, final boolean estPrive, final String description, final int placesDisponibles, final List<Photo> photos, final boolean estAnnule) {
-        creerUnEvenement(String nom, DateTime dateDebut, DateTime dateFin, Lieu localisation, Theme theme, ArrayList<Inscrit> invites, boolean estPrive, String description, int placesDisponibles, ArrayList<Photo> photos, boolean estPrive){
-        
-        
-              //Vérification des différents paramètres
-        
-              Evenement e =  new Evenement(nom, dateDebut, dateFin, ...);
-        
-              Gestion g = g.getInstance();
-        
-               //On ajoute l'événement à la liste totale d'événement du singleton Gestion
-        
-               g.ajouterEvenement(e);
-        
-               //On ajoute l'évenement dans la liste d'événements organisés par l'utilisateur
-        
-               this.evenementsOrganises.add(e);
-        
-        }
-    }
+	public void unfollow(final Inscrit following) {
+	}
 
-    public boolean modifierUnEvenement(final Evenement evtAModifier, final String nom, final Date dateDebut, final Date dateFin, final Lieu localisation, final Theme theme, final List<Inscrit> invites, final boolean estPrive, final String description, final int placesDisponibles, final List<Photo> photos, final boolean estAnnule) {
-        modifierUnEvenement(Evenement evtAModifier, String nom, DateTime dateDebut, DateTime dateFin, Lieu localisation, Theme theme, ArrayList<Inscrit> invites, boolean estPrive, String description, int placesDisponibles, ArrayList<Photo> photos, boolean estPrive){
-        
-        
-              //Vérification des différents paramètres
-        
-              //On appelle tous les setters de l'événement à modifier
-        
-              evtAModifier.setNom(nom);
-        
-              evtAModifier.setDateDebut(dateDebut);
-        
-              //...
-        
-        
-        }
-    }
+	public boolean creerUnEvenement(final String nom, final Date dateDebut, final Date dateFin, final Lieu localisation, final Theme theme, final List<Inscrit> invites, final boolean estPrive, final String description, final int placesDisponibles, final List<Photo> photos, final boolean estAnnule) {
+		creerUnEvenement(String nom, DateTime dateDebut, DateTime dateFin, Lieu localisation, Theme theme, ArrayList<Inscrit> invites, boolean estPrive, String description, int placesDisponibles, ArrayList<Photo> photos, boolean estPrive){
 
-    public void setEmail(final String value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.email = value;
-    }
 
-    public String getEmail() {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        return this.email;
-    }
+			//Vérification des différents paramètres
 
-    public void setNumero(final long value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.numero = value;
-    }
+			Evenement e =  new Evenement(nom, dateDebut, dateFin, ...);
 
-    public long getNumero() {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        return this.numero;
-    }
+			Gestion g = g.getInstance();
 
-    public void setLogin(final String value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.login = value;
-    }
+			//On ajoute l'événement à la liste totale d'événement du singleton Gestion
 
-    public String getLogin() {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        return this.login;
-    }
+			g.ajouterEvenement(e);
 
-    public void setUsername(final String value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.username = value;
-    }
+			//On ajoute l'évenement dans la liste d'événements organisés par l'utilisateur
 
-    public String getUsername() {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        return this.username;
-    }
+			this.evenementsOrganises.add(e);
 
-    public void setPassword(final String value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.password = value;
-    }
+		}
+	}
+	//ROBIN DONE NOT VERIFIED
+	public boolean modifierUnEvenement(final Evenement evtAModifier, final String nom, final Date dateDebut, final Date dateFin, final Lieu localisation, final Theme theme, final List<Inscrit> invites, final boolean estPrive, final String description, final int placesDisponibles, final List<Photo> photos, final boolean estAnnule) {
+		if(evtAModifier.isOrganisateur(this.getLogin())) {
+			
+			evtAModifier.setNom(nom);
+			evtAModifier.setDateDebut(dateDebut);
+			evtAModifier.setDateFin(dateFin);
+			evtAModifier.setLieu(localisation);
+			evtAModifier.setDescription(description);
+			evtAModifier.setTheme(theme);
+			evtAModifier.setParticipants(invites);
+			evtAModifier.setEstAnnule(estAnnule);
+			evtAModifier.setEstPrive(estPrive);
+			evtAModifier.setPhotos(photos);
+			evtAModifier.setPlacesDisponibles(placesDisponibles);
+			evtAModifier.setOrganisateur(this);
+			return true;
+		}
+		return false;
+	}
 
-    public String getPassword() {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        return this.password;
-    }
 
-    public void setDescription(final String value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.description = value;
-    }
+	public void setEmail(final String value) {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		this.email = value;
+	}
 
-    public String getDescription() {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        return this.description;
-    }
+	public String getEmail() {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		return this.email;
+	}
 
-    public List<Theme> getThemesPreferes() {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        return this.themesPreferes;
-    }
+	public void setNumero(final long value) {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		this.numero = value;
+	}
 
-    public void setThemesPreferes(final List<Theme> value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.themesPreferes = value;
-    }
+	public long getNumero() {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		return this.numero;
+	}
 
-    public Photo getPhoto() {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        return this.photo;
-    }
+	public void setLogin(final String value) {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		this.login = value;
+	}
 
-    public void setPhoto(final Photo value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.photo = value;
-    }
+	public String getLogin() {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		return this.login;
+	}
 
-    public List<Inscrit> getFollowings() {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        return this.followings;
-    }
+	public void setUsername(final String value) {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		this.username = value;
+	}
 
-    public void setFollowings(final List<Inscrit> value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.followings = value;
-    }
+	public String getUsername() {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		return this.username;
+	}
 
-    public List<Evenement> getEvenementsOrganises() {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        return this.evenementsOrganises;
-    }
+	public void setPassword(final String value) {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		this.password = value;
+	}
 
-    public void setEvenementsOrganises(final List<Evenement> value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.evenementsOrganises = value;
-    }
+	public String getPassword() {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		return this.password;
+	}
 
-    public List<Evenement> getEvenementsProposes() {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        return this.evenementsProposes;
-    }
+	public void setDescription(final String value) {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		this.description = value;
+	}
 
-    public void setEvenementsProposes(final List<Evenement> value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.evenementsProposes = value;
-    }
+	public String getDescription() {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		return this.description;
+	}
 
-    public List<Evenement> getEvenementsParticipes() {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        return this.evenementsParticipes;
-    }
+	public List<Theme> getThemesPreferes() {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		return this.themesPreferes;
+	}
 
-    public void setEvenementsParticipes(final List<Evenement> value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.evenementsParticipes = value;
-    }
+	public void setThemesPreferes(final List<Theme> value) {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		this.themesPreferes = value;
+	}
 
-    public void setFollowers(final List<Inscrit> value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.followers = value;
-    }
+	public Photo getPhoto() {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		return this.photo;
+	}
+
+	public void setPhoto(final Photo value) {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		this.photo = value;
+	}
+
+	public List<Inscrit> getFollowings() {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		return this.followings;
+	}
+
+	public void setFollowings(final List<Inscrit> value) {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		this.followings = value;
+	}
+
+	public List<Evenement> getEvenementsOrganises() {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		return this.evenementsOrganises;
+	}
+
+	public void setEvenementsOrganises(final List<Evenement> value) {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		this.evenementsOrganises = value;
+	}
+
+	public List<Evenement> getEvenementsProposes() {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		return this.evenementsProposes;
+	}
+
+	public void setEvenementsProposes(final List<Evenement> value) {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		this.evenementsProposes = value;
+	}
+
+	public List<Evenement> getEvenementsParticipes() {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		return this.evenementsParticipes;
+	}
+
+	public void setEvenementsParticipes(final List<Evenement> value) {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		this.evenementsParticipes = value;
+	}
+
+	public void setFollowers(final List<Inscrit> value) {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		this.followers = value;
+	}
 
 }
--- src/system/Gestion.java
+++ src/system/Gestion.java
@@ -10,240 +10,195 @@ import model.Photo;
 import model.Theme;
 
 public class Gestion {
-    private List<Inscrit> inscrits = new ArrayList<Inscrit> ();
+	private List<Inscrit> inscrits = new ArrayList<Inscrit> ();
 
-    private static Gestion Instance;
+	private static Gestion Instance;
 
-    public List<Evenement> evenements = new ArrayList<Evenement> ();
+	public List<Evenement> evenements = new ArrayList<Evenement> ();
 
-    private Gestion() {
-    }
+	private Gestion() {
+	}
 
-    public static Gestion getInstance() {
-        return Instance;
-    }
+	public static Gestion getInstance() {
+		return Instance;
+	}
+	//ROBIN DONE NOT VERIFIED
+	public boolean sInscrire(final String email, final String login, final String username, final String password) {
+		if(checkEmail(email) && checkLogin(login) && checkUsername(username) && checkPasswd(password)){
 
-    public boolean sInscrire(final String email, final String login, final String username, final String password) {
-              if(checkEmail(email) && checkLogin(login) && checkUsername(username) && checkPasswd(password)){
-        
-                    this.inscrits.add(new Inscrit(email, login, username, password));
-        
-                    return true;
-        
-              } else{
-        
-                   return false;
-        
-             }
-        
-    }
+			this.inscrits.add(new Inscrit(email, login, username, password));
 
-    public boolean sInscrire(final long numero, final String login, final String username, final String password) {
-        // TODO Auto-generated return
-        return false;
-    }
+			return true;
 
-    public boolean checkEmail(final String email) {
-        // TODO Auto-generated return
-        return false;
-    }
-    
-    //ROBIN
-    public boolean checkNumber(final long number) {
-        // TODO Auto-generated return
-        return false;
-    }
+		} else{
 
-  //ROBIN DONE NOT CHECKED
-  	public boolean checkEmail(final String email) {
-  		if( email!=null && email.trim().length()>0 )
-  			return email.matches("^[a-zA-Z0-9\\.\\-\\_]+@([a-zA-Z0-9\\-\\_\\.]+\\.)+([a-zA-Z]{2,4})$");
-  		return false;
-  	}
+			return false;
+
+		}
+
+	}
+	//ROBIN DONE NOT VERIFIED
+	public boolean sInscrire(final long numero, final String login, final String username, final String password) {
+		if(checkNumber(numero) && checkLogin(login) && checkUsername(username) && checkPasswd(password)){
+
+			this.inscrits.add(new Inscrit(numero, login, username, password));
+
+			return true;
+
+		} else{
+
+			return false;
+
+		}
+	}
+
+	public boolean checkEmail(final String email) {
+		// TODO Auto-generated return
+				return false;
+	}
+
+	//ROBIN
+	public boolean checkNumber(final long number) {
+		// TODO Auto-generated return
+				return false;
+	}
+
+	//ROBIN DONE NOT CHECKED
+	public boolean checkEmail(final String email) {
+		if( email!=null && email.trim().length()>0 )
+			return email.matches("^[a-zA-Z0-9\\.\\-\\_]+@([a-zA-Z0-9\\-\\_\\.]+\\.)+([a-zA-Z]{2,4})$");
+		return false;
+	}
 
 
-  	//ROBIN DONE NOT CHECKED
-  	public boolean checkLogin(final String login) {
-  		for (Inscrit inscrit : inscrits) {
-  			if (inscrit.getLogin()==login) {
-  				return false;
-  			}
-  		}
-  		return true;
-  	}
+	//ROBIN DONE NOT CHECKED
+	public boolean checkLogin(final String login) {
+		for (Inscrit inscrit : inscrits) {
+			if (inscrit.getLogin()==login) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-  	//ROBIN DONE NOT CHECKED
-  	public boolean checkUsername(final String username) {
-  		for (Inscrit inscrit : inscrits) {
-  			if (inscrit.getUsername()==username) {
-  				return false;
-  			}
-  		}
-  		return true;
-  	}
-  	//ROBIN DONE NOT CHECKED
-  	public boolean checkPasswd(final String passwd) {
-  		if( passwd!=null && passwd.trim().length()>0 )   //true
-  			if (passwd.matches("^@([a-zA-Z0-9])$") ) {   //true
-  				for (Inscrit inscrit : inscrits) {
-  					if (inscrit.getPassword()==passwd) { //false !
-  						return false;
-  					}
-  				}
-  			}
-  		return false;
+	//ROBIN DONE NOT CHECKED
+	public boolean checkUsername(final String username) {
+		for (Inscrit inscrit : inscrits) {
+			if (inscrit.getUsername()==username) {
+				return false;
+			}
+		}
+		return true;
+	}
+	//ROBIN DONE NOT CHECKED
+	public boolean checkPasswd(final String passwd) {
+		if( passwd!=null && passwd.trim().length()>0 )   //true
+			if (passwd.matches("^@([a-zA-Z0-9])$") ) {   //true
+				for (Inscrit inscrit : inscrits) {
+					if (inscrit.getPassword()==passwd) { //false !
+						return false;
+					}
+				}
+			}
+		return false;
 
-  	}
+	}
 
-    //Héloïse
-    public boolean seConnecter(final String email, final String password) {
-        Inscrit inscrit = rechercherProfilParEmail(email);
+	//Héloïse
+	public boolean seConnecter(final String email, final String password) {
+		Inscrit inscrit = rechercherProfilParEmail(email);
 
-        if(inscrit != null){
-              if(checkPasswordConcordance(password, inscrit)){
-                     return true;
-              } else{
-                    return false;
-              }
-        } else{
-             System.err.println("Ce mail ne correspond à aucun membre existant !");
-             return false;
-        }        
-    }
+		if(inscrit != null){
+			if(checkPasswordConcordance(password, inscrit)){
+				return true;
+			} else{
+				return false;
+			}
+		} else{
+			System.err.println("Ce mail ne correspond à aucun membre existant !");
+			return false;
+		}        
+	}
 
-    //Héloïse
-    public boolean seConnecter(final long numero, final String password) {
-    	Inscrit inscrit = rechercherProfilParNumero(numero);
-        if(inscrit != null){
-              if(checkPasswordConcordance(password, inscrit)){
-                     return true;
-              } else{
-                    return false;
-              }
-        } else{
-             System.err.println("Ce mail ne correspond à aucun membre existant !");
-             return false;
-        }
-    }
+	//Héloïse
+	public boolean seConnecter(final long numero, final String password) {
+		Inscrit inscrit = rechercherProfilParNumero(numero);
+		if(inscrit != null){
+			if(checkPasswordConcordance(password, inscrit)){
+				return true;
+			} else{
+				return false;
+			}
+		} else{
+			System.err.println("Ce mail ne correspond à aucun membre existant !");
+			return false;
+		}
+	}
 
-    //Héloïse
-    public boolean checkPasswordConcordance(final String passwordEntered, final Inscrit inscrit) {
-       if(inscrit.getPassword().equals(passwordEntered)){
-             return true;
-       }else{
-             System.err.println("Le mot de passe ne correspond pas !");
-             return false;
-        }
-    }
+	//Héloïse
+	public boolean checkPasswordConcordance(final String passwordEntered, final Inscrit inscrit) {
+		if(inscrit.getPassword().equals(passwordEntered)){
+			return true;
+		}else{
+			System.err.println("Le mot de passe ne correspond pas !");
+			return false;
+		}
+	}
 
-    public void afficherProfil(final Inscrit inscrit) {
-    }
+	public void afficherProfil(final Inscrit inscrit) {
+	}
 
-    public boolean modifierUnProfil(final Inscrit profilAModifier, final String email, final long numero, final String username, final String passwd, final String description) {
-        // TODO Auto-generated return
-        return false;
-    }
+	public boolean modifierUnProfil(final Inscrit profilAModifier, final String email, final long numero, final String username, final String passwd, final String description) {
+		// TODO Auto-generated return
+		return false;
+	}
 
-    public void participerAEvenement(final Evenement evenement, final Inscrit participant) {
-    }
+	public void participerAEvenement(final Evenement evenement, final Inscrit participant) {
+	}
 
-    public void afficherEvenement(final Evenement evenement) {
-        System.out.println(e);
-        
-        //Demande Ã  l'utilisateur s'il veut participer Ã  l'Ã©vÃ©nement
-        
-        //Si true
-        
-        //Appel Ã  participerEvenement()
-        
-    }
+	public void afficherEvenement(final Evenement evenement) {
+		System.out.println(e);
 
-    public void ajouterEvenement(final Evenement e) {
-               this.evenements.add(e);
-    }
+		//Demande Ã  l'utilisateur s'il veut participer Ã  l'Ã©vÃ©nement
 
-    public boolean rechercherUnEvenement(final String nom, final Date dateDebut, final Date dateFin, final Lieu localisation, final Theme theme, final List<Inscrit> invites, final boolean estPrive, final String description, final int placesDisponibles, final List<Photo> photos, final boolean estAnnule) {
-        // TODO Auto-generated return
-        return false;
-    }
+		//Si true
 
-    //Héloïse
-    public Inscrit rechercherProfilParUsername(final String username) {
-    	Inscrit i = new Inscrit();
-    	for(Inscrit inscrit : inscrits) {
-    		if(inscrit.getUsername() == username) {
-    			i = inscrit;
-    		}
-    		else {
-    			i = null;
-    		}
-    	}
-    	return i;
-    }
+		//Appel Ã  participerEvenement()
 
-    //Héloïse
-    public Inscrit rechercherProfilParEmail(final String email) {
-    	Inscrit i = new Inscrit();
-    	for(Inscrit inscrit : inscrits) {
-    		if(inscrit.getEmail() == email) {
-    			i = inscrit;
-    		}
-    		else {
-    			i = null;
-    		}
-    	}
-    	return i;
-    }
+	}
 
-    //Héloïse
-    public Inscrit rechercherProfilParLogin(final String login) {
-    	Inscrit i = new Inscrit();
-    	for(Inscrit inscrit : inscrits) {
-    		if(inscrit.getLogin() == login) {
-    			i = inscrit;
-    		}
-    		else {
-    			i = null;
-    		}
-    	}
-    	return i;
-    }
+	public void ajouterEvenement(final Evenement e) {
+		this.evenements.add(e);
+	}
 
-    //Héloïse
-    public Inscrit rechercherProfilParNumero(final long numero) {
-    	Inscrit i = new Inscrit();
-    	for(Inscrit inscrit : inscrits) {
-    		if(inscrit.getNumero() == numero) {
-    			i = inscrit;
-    		}
-    		else {
-    			i = null;
-    		}
-    	}
-    	return i;
-    }
+	public boolean rechercherUnEvenement(final String nom, final Date dateDebut, final Date dateFin, final Lieu localisation, final Theme theme, final List<Inscrit> invites, final boolean estPrive, final String description, final int placesDisponibles, final List<Photo> photos, final boolean estAnnule) {
+		// TODO Auto-generated return
+		return false;
+	}
 
-    public boolean envoyerUnEmail(final String email, final String message) {
-        // TODO Auto-generated return
-        return false;
-    }
 
-    public boolean envoyerUnSMS(final long num, final String message) {
-        // TODO Auto-generated return
-        return false;
-    }
 
-    public void ajouterInscrit(final Inscrit i) {
-    }
+	public boolean envoyerUnEmail(final String email, final String message) {
+		// TODO Auto-generated return
+		return false;
+	}
 
-    public Evenement getEvenement() {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        return this.evenement;
-    }
+	public boolean envoyerUnSMS(final long num, final String message) {
+		// TODO Auto-generated return
+		return false;
+	}
 
-    public void setEvenement(final Evenement value) {
-        // Automatically generated method. Please delete this comment before entering specific code.
-        this.evenement = value;
-    }
+	public void ajouterInscrit(final Inscrit i) {
+	}
+
+	public Evenement getEvenement() {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		return this.evenement;
+	}
+
+	public void setEvenement(final Evenement value) {
+		// Automatically generated method. Please delete this comment before entering specific code.
+		this.evenement = value;
+	}
 
 }
